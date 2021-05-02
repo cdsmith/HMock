d8691d5d696755e0492a0df31dfe85543d9f036b
Chris Smith <cdsmith@gmail.com>
2021-05-02 08:45:19 -0400

    cleanup

--- test/TH.hs
+++ test/TH.hs
@@ -24,24 +24,30 @@ class MonadFoo1 m where
 
 makeMockable [t|MonadFoo1|]
 
-class MonadFoo2 a m | m -> a where
+class MonadFoo2 a m where
   foo2 :: a -> m ()
 
-newtype Foo2IntM m a = Foo2IntM {runFoo2IntM :: m a}
-  deriving (Functor, Applicative, Monad)
-
-deriveMockableWithOptions def {mockPrefix = "Int"} [t|MonadFoo2 Int|]
-
-instance (Typeable m, Monad m) => MonadFoo2 Int (MockT (Foo2IntM m)) where
-  foo2 x = mockMethod (IntFoo2 x)
-
-newtype Foo2StrM m a = Foo2StrM {runFoo2StrM :: m a}
-  deriving (Functor, Applicative, Monad)
-
-deriveMockableWithOptions def {mockPrefix = "Str"} [t|MonadFoo2 String|]
-
-instance (Typeable m, Monad m) => MonadFoo2 String (MockT (Foo2StrM m)) where
-  foo2 x = mockMethod (StrFoo2 x)
+instance Typeable a => Mockable (MonadFoo2 a) where
+  data Action (MonadFoo2 a) :: Type -> Type
+    where Foo2 :: a -> Action (MonadFoo2 a) ()
+  data Matcher (MonadFoo2 a) :: Type -> Type
+    where Foo2_ :: Predicate a -> Matcher (MonadFoo2 a) ()
+  showAction (Foo2 a_aHW45)
+    = unwords ("foo2" : ["_ :: a"])
+  showMatcher (Just (Foo2 a_aHW48)) (Foo2_ p_aHW4a)
+    = unwords ("foo2" : ["«" ++ (showPredicate p_aHW4a ++ "»")])
+  showMatcher Nothing (Foo2_ p_aHW4a)
+    = unwords ("foo2" : ["«" ++ (showPredicate p_aHW4a ++ "»")])
+  match (Foo2_ p_aHW4b) (Foo2 a_aHW4c)
+    | mismatches_aHW4d == 0 = FullMatch Refl
+    | otherwise = PartialMatch Refl mismatches_aHW4d
+    where
+        mismatches_aHW4d = length (filter not [accept p_aHW4b a_aHW4c])
+instance (Typeable a, Eq a, Show a) => ExactMockable (MonadFoo2 a) where
+  exactly (Foo2 a_aHW4e) = Foo2_ (eq_ a_aHW4e)
+instance (Typeable a, Typeable m_aHW4f, Monad m_aHW4f) =>
+         MonadFoo2 a (MockT m_aHW4f) where
+  foo2 a_aHW4g = mockMethod (Foo2 a_aHW4g)
 
 class MonadFoo3 m where
   foo3 :: Enum a => String -> a -> b -> m ()
