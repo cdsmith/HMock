692cae019388ef88db5499a42aef9af02beedc93
Chris Smith <cdsmith@gmail.com>
2021-05-16 23:21:51 -0400

    Move doctests to a new test suite

--- test/Main.hs
+++ test/Main.hs
@@ -1,22 +1,11 @@
 import Classes (classTests)
 import Core (coreTests)
 import Extras (multiplicityTests, predicateTests)
-import Test.DocTest (doctest)
 import Test.Hspec (hspec)
 
-runDoctest :: IO ()
-runDoctest =
-  doctest
-    [ "-isrc",
-      "src/Test/HMock/Internal/Predicates.hs",
-      "src/Test/HMock/Internal/Multiplicity.hs"
-    ]
-
 main :: IO ()
-main = do
-  runDoctest
-  hspec $ do
-    multiplicityTests
-    predicateTests
-    coreTests
-    classTests
+main = hspec $ do
+  multiplicityTests
+  predicateTests
+  coreTests
+  classTests
