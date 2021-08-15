bc674dc63c676224af4db2a03e50f37e45382057
Chris Smith <cdsmith@gmail.com>
2021-08-15 13:06:41 -0400

    Flow-based matcher implementation.  See #17

--- test/DocTests/All.hs
+++ test/DocTests/All.hs
@@ -1,6 +1,7 @@
 -- Do not edit! Automatically created with doctest-extract.
 module DocTests.All where
 
+import qualified DocTests.Test.HMock.Internal.FlowMatcher
 import qualified DocTests.Test.HMock.Predicates
 import qualified DocTests.Test.HMock.Multiplicity
 
@@ -8,5 +9,6 @@ import qualified Test.DocTest.Driver as DocTest
 
 main :: DocTest.T ()
 main = do
+    DocTests.Test.HMock.Internal.FlowMatcher.test
     DocTests.Test.HMock.Predicates.test
     DocTests.Test.HMock.Multiplicity.test
